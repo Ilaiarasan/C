242e71d6d59b469b04ca443dda639e68048528ba
Ilaiarasan <37308119+Ilaiarasan@users.noreply.github.com>
2018-03-13 23:11:24 +0530

    Update additon.c

--- additon.c
+++ additon.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
-int d;
-int main()
+#include<stdio.h>                            //header file
+int d;                                       //global variable declaration
+int main()                                   //main funnction
 {
-int a,b,c,sum;
-printf("adition of 2 numbers\n");
-
-scanf("%d%d",&a,&b);
-sum=a+b;
-printf("%d %d %d %d %d\n",a ,b,sum,c,d);
+  int a,b,c,sum;                             //local variable declaration
+  printf("adition of 3 numbers\n");          //print statement
+  scanf("%d%d%d",&a,&b,&c);                 //input statement
+  sum=a+b+c;                            
+  printf("%d\n",sum);                       //print statement
+  printf("%d\n",d);                         //global variable declaration
+  return 0;                                 //return statement
 }
